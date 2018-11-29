a2c4f35f5e3232d0df889fbc892b4b6bd8d3bfee
Pedro Henrique Rogge <pedrorogge@hotmail.com>
2018-11-29 20:21:54 -0200

    alteracoes em casa

--- Feedback360/Controllers/HomeController.cs
+++ Feedback360/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Feedback360.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +12,40 @@ namespace Feedback360.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            ComandosBancoDeDados comandos = new ComandosBancoDeDados();
+            var listaDePessoas = comandos.BuscarPessoas();
+            return View(listaDePessoas);
         }
+        public ActionResult CarregarFeed(Guid pessoaId)
+        {
+            ComandosBancoDeDados comandos = new ComandosBancoDeDados();
+            var pessoa = comandos.BuscarPessoaPorPessoaId(pessoaId);
+            var manter = comandos.BuscarManterPorPessoaId(pessoaId);
+            var mudar = comandos.BuscarMudarPorPessoaId(pessoaId);
+            var melhorar = comandos.BuscarMelhorarPorPessoaId(pessoaId);
 
+            FeedbackCompleto feedback = new FeedbackCompleto();
+            feedback.Pessoa = pessoa;
+            feedback.Manter = manter;
+            feedback.Mudar = mudar;
+            feedback.Melhorar = melhorar;
+
+            if (feedback.Manter.Count > feedback.Mudar.Count
+                && feedback.Manter.Count > feedback.Melhorar.Count)
+                feedback.MaxCount = feedback.Manter.Count;
+
+            if (feedback.Mudar.Count > feedback.Manter.Count
+               && feedback.Mudar.Count > feedback.Melhorar.Count)
+                feedback.MaxCount = feedback.Mudar.Count;
+
+            if (feedback.Melhorar.Count > feedback.Manter.Count
+               && feedback.Melhorar.Count > feedback.Mudar.Count)
+            {
+                feedback.MaxCount = feedback.Melhorar.Count;
+            }
+
+
+            return View(feedback);
+        }
     }
 }
---
+++ Feedback360/Models/FeedbackCompleto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Feedback360.Models
+{
+    public class FeedbackCompleto
+    {
+        
+            public Pessoa Pessoa { get; set; }
+            public List<Melhorar> Melhorar { get; set; }
+            public List<Mudar> Mudar { get; set; }
+            public List<Manter> Manter { get; set; }
+            public int MaxCount { get; set; }
+        
+    }
+}
