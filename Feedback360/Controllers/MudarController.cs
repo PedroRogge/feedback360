87af867c2db64bd5d441c21157ef5b01fc2b9361
Pedro Henrique Rogge <pedrorogge@hotmail.com>
2018-12-04 21:25:39 -0200

    inclusao ignore

---
+++ Feedback360/Controllers/MudarController.cs
@@ -0,0 +1,52 @@
+﻿using Feedback360.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feedback360.Controllers
+{
+    public class MudarController : Controller
+    {
+        public ComandosBancoDeDados Comandos { get; set; }
+
+        public MudarController()
+        {
+            Comandos = new ComandosBancoDeDados();
+        }
+
+        // GET: Manter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CriarMudar()
+        {
+            PreencherViewBagPessoa();
+            return View();
+        }
+
+        public ActionResult SalvarMudar(Mudar mudar)
+        {
+            if (ModelState.IsValid)
+            {
+                Comandos.InserirFeedbackMudar(mudar);
+                return RedirectToRoute(new
+                {
+                    controller = "Feedback",
+                    action = "Carregar",
+                    pessoaId = mudar.PessoaId
+                });
+            }
+            PreencherViewBagPessoa();
+            return View("CriarMudar", mudar);
+        }
+
+        public void PreencherViewBagPessoa()
+        {
+            ViewBag.Pessoas = Comandos.BuscarPessoas();
+        }
+    }
+}
--- Feedback360/Models/Mudar.cs
+++ Feedback360/Models/Mudar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,7 +14,12 @@ namespace Feedback360.Models
         }
 
         public Guid MudarId { get; set; }
+
+        [Required(ErrorMessage = "O Observação é obrigatório")]
+        [MinLength(5, ErrorMessage = "Observação deve ser maior ou igual a 5 caracteres")]
         public string Observacao { get; set; }
+
+        [Required(ErrorMessage = "Campo pessoa é obrigatório")]
         public Guid PessoaId { get; set; }
 
     }
